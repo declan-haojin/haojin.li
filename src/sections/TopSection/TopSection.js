9b3abcec1cf067077caeb721ddb734899bc959b2
Haojin Li <hi@haojin.li>
2024-01-26 01:54:12 -0500

    Remove subheadline

--- src/sections/TopSection/TopSection.js
+++ src/sections/TopSection/TopSection.js
@@ -23,7 +23,7 @@ class TopSection extends React.Component {
                 <div className="intro">
                     <h1>{CommonConfig.name}</h1>
                     <p>{CommonConfig.tagline}</p>
-                    <h4>because one major alone wasn’t enough to confuse me</h4>
+                    {/* <h4>because one major alone wasn’t enough to confuse me</h4> */}
                 </div>
 
                 <div className="social">
